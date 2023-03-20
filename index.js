ef57478198c302f3e6d5d4af35f1e96f78a856a6
Inda Smith <inda.k.smith@gmail.com>
2023-03-20 15:22:35 -0400

    distance in blocks passing, nothing else is

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) { 
+        return (blocks - 42)
+    }
+    if (blocks < 42) {
+        return (42 - blocks)
+    }
+}
+function distanceFromHqInFeet(feet) {
+    distanceFromHqInBlocks(blocks)
+    return feet (blocks * 264);
+}
+function distanceTravelledInFeet(start, destination) {
+
+}
+function calculatesFarePrice(start, destination) {
+
+}
