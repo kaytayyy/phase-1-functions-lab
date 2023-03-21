1395bd4eea2a3ba31a8aa098879fb8730dadd5ed
Inda Smith <inda.k.smith@gmail.com>
2023-03-21 19:48:07 -0400

    first three tests are passing, the res t still aren't. made some adjustments to the second line of code , getting numbers now, though they are not calculating properly.  something wrong with the first block of code.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks) {
-    if (blocks > 42) { 
-        return (blocks - 42)
+   if (blocks > 42) { 
+    return (blocks - 42)
+
     }
-    if (blocks < 42) {
+    else if (blocks < 42) {
         return (42 - blocks)
     }
+    console.log()
+    //something wrong with this block of code.
+    //calculation isn't correct.
 }
-function distanceFromHqInFeet(feet) {
-    distanceFromHqInBlocks(blocks)
-    return feet (blocks * 264);
+function distanceFromHqInFeet(distanceFromHqInBlocks) {
+    
+    console.log(distanceFromHqInBlocks * 264)
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function distanceTravelledInFeet(start, destination) {
 
 }
