e3cae85b77565bd3981426defa1b900ef9613edc
Inda Smith <inda.k.smith@gmail.com>
2023-03-22 11:12:29 -0400

    I only have 5 tests left to pass, made adjustments to the feet functions

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(blocks) {
-   if (blocks > 42) { 
-    return (blocks - 42)
+    if (blocks > 42) { 
+    return blocks - 42
 
     }
     else if (blocks < 42) {
-        return (42 - blocks)
+        return 42 - blocks
     }
+
     console.log()
-    //something wrong with this block of code.
-    //calculation isn't correct.
-}
-function distanceFromHqInFeet(distanceFromHqInBlocks) {
-    
-    console.log(distanceFromHqInBlocks * 264)
-    
-    
-    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function distanceFromHqInFeet(distance) {
+    const n = 
+    distanceFromHqInBlocks(distance);
+    return n * 264   
+}
 
 function distanceTravelledInFeet(start, destination) {
-
+    const i =
+    destination - start
+    return i * 264
 }
 function calculatesFarePrice(start, destination) {
 
